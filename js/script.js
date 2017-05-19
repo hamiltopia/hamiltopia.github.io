160ee4c870a3141fa419b3c5429213171a50332f
Mike Hamilton <mike_hamilton@wgbh.org>
2017-05-19 16:29:18 -0400

    update to header

--- js/script.js
+++ js/script.js
@@ -1,13 +1,5 @@
 $(document).ready(function(){
 
-  function autoresize(textarea) {
-    textarea.style.height = '0px';     //Reset height, so that it not only grows but also shrinks
-    textarea.style.height = (textarea.scrollHeight+10) + 'px';    //Set new height
-  }
-
-  $('.autosize').keyup(function () {
-    autoresize(this);
-  });
 
 //end
 });
